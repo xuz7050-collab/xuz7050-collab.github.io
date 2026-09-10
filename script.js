de09919ecb233411fbe7809dc704a089498d342d
xuz7050-collab <xuz7050@gmail.com>
2026-09-10 22:20:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const menuBtn=document.querySelector(".menu-btn");const nav=document.querySelector(".nav-links");if(menuBtn){menuBtn.addEventListener("click",()=>nav.classList.toggle("open"));document.querySelectorAll(".nav-links a").forEach(a=>a.addEventListener("click",()=>nav.classList.remove("open")))}
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",e=>{const el=document.querySelector(a.getAttribute("href"));if(el){e.preventDefault();el.scrollIntoView({behavior:"smooth"})}}));
